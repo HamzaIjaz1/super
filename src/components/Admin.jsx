f2067f3646e30fabeff57d005a827a1c28a0f147
HamzaIjaz1 <iamhamzza@gmail.com>
2019-06-01 20:01:27 +0500

    conditional routing

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,100 +1,125 @@
-import React, { Component } from 'react';
-import {Layout, Menu, Icon, Button } from 'antd';
+import React, { Component } from "react";
+import { Layout, Menu, Icon, Button } from "antd";
+import Graph from './Graph';
+import Users from './users';
 import logo from "../images/logo.png";
 
-import MenuItem from 'antd/lib/menu/MenuItem';
 
+
+import MenuItem from "antd/lib/menu/MenuItem";
 
 class Admin extends Component {
-      
-    state = {
-        collapsed: false,
-      }
-      onCollapse = collapsed => {
-        console.log(collapsed);
-        this.setState({ collapsed });
-      };
-      
-    
-    render() {
-      const { Header, Content, Footer, Sider } = Layout;
-      const SubMenu = Menu.SubMenu;
-              
-               
-        return (  
+  state = {
+    collapsed: false,
+    rendering:''
+  };
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    const { Header, Content, Footer, Sider } = Layout;
+    const SubMenu = Menu.SubMenu;
+
+    return (
+      <div>
+        <Layout style={{ minHeight: '100%'}}>
+          <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={broken => {
+              console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+              console.log(collapsed, type);
+            }}
+          >
             <div>
-                <Layout>
-                <Sider
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={broken => {
-                console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                console.log(collapsed, type);
-                 }}
-          >
-                 <div   > 
-               <img src={logo} alt="logo" style={{height: '50px',margin: '16px',}}/>
-                 </div>
-                 <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-          inlineCollapsed={this.state.collapsed}
-        >
-         <Menu.Item key="1">
-            <Icon type="pie-chart" />
-            <span> User Activities</span>
-          </Menu.Item>
-          
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-               <Icon type="pie-chart" />
-                <span>Store Activities</span>
-              </span>
-            }
-          >
-            <Menu.Item key="2">New Products</Menu.Item>
-            <Menu.Item key="3">New Stores</Menu.Item>
-            <Menu.Item key="4">New Reviews</Menu.Item>
-            
-          </SubMenu>
-          <Menu.Item key="5">
-            <Icon type="desktop" />
-            <span>Users</span>
-          </Menu.Item>
-          <Menu.Item key="6" onClick={this.handlestores}>
-            <Icon type="inbox" />
-            <span>Stores</span>
-          </Menu.Item>
-
-          <Menu.Item key="7">
-            <Icon type="inbox" />
-            <span>Notifications</span>
-          </Menu.Item>
-            
-          </Menu>
-          </Sider>
-    <Layout>
-      <Header style={{ background: '#fff', padding: 0 }} >
-        <h1 style={{fontFamily:'sans sarif',}}>DashBoard</h1>
-      </Header>
-
-      <Content style={{ margin: '24px 16px 0' }}>
-        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-        
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    </Layout>
-  </Layout>,
+              <img
+                src={logo}
+                alt="logo"
+                style={{ height: "50px", margin: "16px" }}
+              />
             </div>
-        );
-    }
+            <Menu
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              mode="inline"
+              theme="dark"
+              inlineCollapsed={this.state.collapsed}
+            >
+              <Menu.Item key="1">
+                <Icon type="pie-chart" />
+                <span onClick={()=>{this.setState({rendering:'graph'})}}> Activities</span>
+              </Menu.Item>
+
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="pie-chart" />
+                    <span>Store Activities</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="2">New Products</Menu.Item>
+                <Menu.Item key="3">New Stores</Menu.Item>
+                <Menu.Item key="4">New Reviews</Menu.Item>
+              </SubMenu>
+              <Menu.Item key="5">
+                <Icon type="desktop" />
+                <span onClick={()=>{this.setState({rendering:'users'})}}>Users</span>
+              </Menu.Item>
+              <Menu.Item key="6" onClick={this.handlestores}>
+                <Icon type="inbox" />
+                <span>Stores</span>
+              </Menu.Item>
+
+              <Menu.Item key="7">
+                <Icon type="inbox" />
+                <span>Notifications</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{ background: "#fff", padding: 0 }}>
+              <h1 style={{ fontFamily: "sans sarif" }}>DashBoard</h1>
+            </Header>
+
+            <Content style={{ margin: "24px 16px 0" }}>
+              <div
+                style={{ padding: 24, background: "#fff", minHeight: 360 }}
+              >
+                Main content to be shown here
+                {this.state.rendering === 'graph' && <Graph/>}
+                {this.state.rendering == 'users' && <Users/>}
+
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              Ant Design ©2018 Created by Ant UED
+              <br/>
+              <div>Something</div>
+              <br/>
+              <div>Something</div>
+              Ant Design ©2018 Created by Ant UED
+              <br/>
+              <div>Something</div>
+              <br/>
+              <div>Something</div>
+              Ant Design ©2018 Created by Ant UED
+              <br/>
+              <div>Something</div>
+              <br/>
+              <div>Something</div>
+            </Footer>
+          </Layout>
+        </Layout>
+        ,
+      </div>
+    );
+  }
 }
- 
+
 export default Admin;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
 import 'antd/dist/antd';
-import Admin from './components/Admin';
-import Graph from './components/Graph';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
 
-ReactDOM.render(<Graph />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
